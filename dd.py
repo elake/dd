df86182ddb2336fa89ab96149488bf629a0c84ae
elake <elake@ualberta.ca>
2020-11-28 19:13:51 -0700

    Update dd.py

--- dd.py
+++ dd.py
@@ -1,21 +1,27 @@
 import ipaddress
 import time
 
-d = {}
-s = set()
-begintime = time.time()
+def main():
 
-for i in range(2):
-    for j in range(2):
-        for k in range(2):
-            for l in range(2):
-                ipa = str(i) + '.' + str(j) + '.' + str(k) + '.' + str(l)  
-                iface = ipaddress.IPv4Interface(ipa)
-                ipas = ipa.replace('.', '')
-                d[iface] = ipas
+    d = {}
+    s = set()
+    begintime = time.time()
+    d["newkey"] = 7
 
-# print(hash(iface))
-endtime = time.time()
-elapsed = endtime - begintime
-print("All done")
-print(elapsed)
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                for l in range(2):
+                    ipa = str(i) + '.' + str(j) + '.' + str(k) + '.' + str(l)  
+                    iface = ipaddress.IPv4Interface(ipa)
+                    ipas = ipa.replace('.', '')
+                    d[iface] = ipas
+
+    # print(hash(iface))
+    endtime = time.time()
+    elapsed = endtime - begintime
+    print("All done")
+    print(elapsed)
+
+if __name__ == "__main__":
+    main()
